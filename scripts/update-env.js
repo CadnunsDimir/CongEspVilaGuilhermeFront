1fe1ab7e1547da8a0b48f514ec88400eb970c964
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2025-04-21 13:20:12 -0300

    chore(CD): add env geneare script

---
+++ scripts/update-env.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `
+export const environment = {
+  production: true,
+  apiUrl: '${process.env.API_URL}'
+};
+`;
+
+fs.writeFile(targetPath, envConfigFile, (err) => {
+  if (err) {
+    console.error('Erro ao escrever o arquivo de ambiente:', err);
+  } else {
+    console.log('Arquivo de ambiente criado com sucesso!');
+  }
+});
