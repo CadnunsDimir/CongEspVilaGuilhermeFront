0332a6d96eec7480cc73666b71da0a058bba95c6
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2025-07-29 23:49:11 -0300

    feat(preaching-schedule): add special days

--- src/app/pages/preaching-schedule/preaching-schedule.component.ts
+++ src/app/pages/preaching-schedule/preaching-schedule.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PreachingScheduleService } from '../../services/preaching-schedule/preaching-schedule.service';
 import { map } from 'rxjs';
-import { PreachingScheduleDay } from '../../models/preaching-schedule.model';
+import { PreachingScheduleDay, SpecialPreachingDay } from '../../models/preaching-schedule.model';
 
 @Component({
   selector: 'app-preaching-schedule',
@@ -10,41 +10,44 @@ import { PreachingScheduleDay } from '../../models/preaching-schedule.model';
 })
 export class PreachingScheduleComponent implements OnInit {
   
-  
-  fixedDays$ = this.preachingSchedule.data$.pipe(
-    map((data) => data.fixedPreachingDays));
-    
-    monthAndYear?: string;
-    editFixedDay: PreachingScheduleDay | null = null;
-    
-    constructor(private readonly preachingSchedule: PreachingScheduleService) {
-      
-    }
-    
-    ngOnInit(): void {      
-      const { month, year } = this.getMonthAndYear();      
-      this.monthAndYear = `${ month }/${ year }`;
+  fixedDays$ = this.preachingSchedule.fixedDays$;
+  specialDays$ = this.preachingSchedule.specialDays$;
+
+  monthAndYear?: string;
+  editFixedDay: PreachingScheduleDay | null = null;
+
+  constructor(private readonly preachingSchedule: PreachingScheduleService) {
+
+  }
+
+  ngOnInit(): void {
+    const { month, year } = this.getMonthAndYear();
+    this.monthAndYear = `${month}/${year}`;
+  }
+
+  getMonthAndYear(): { month: any; year: any; } {
+    const date = new Date();
+
+    if (date.getDate() > 20) {
+      date.setMonth(date.getMonth() + 1);
+      date.setDate(1);
     }
 
-    getMonthAndYear(): { month: any; year: any; } {
-      const date = new Date();
-        
-      if (date.getDate() > 20) {
-        date.setMonth(date.getMonth() + 1);
-        date.setDate(1);      
-      }
-        
-      const month = date.toLocaleString('es-ES', { month: 'long'});
-      const year = date.getFullYear();
-      
-      return { month, year }
-    }
-    
-    onEditFixedDay(fixedDay: PreachingScheduleDay) {
-      this.editFixedDay = fixedDay;
-    }
-    
-    saveFixedDay(fixedDay: PreachingScheduleDay) {
-      this.preachingSchedule.updateFixedDay(fixedDay).subscribe();
-    }
+    const month = date.toLocaleString('es-ES', { month: 'long' });
+    const year = date.getFullYear();
+
+    return { month, year }
+  }
+
+  onEditFixedDay(fixedDay: PreachingScheduleDay) {
+    this.editFixedDay = fixedDay;
+  }
+
+  saveFixedDay(fixedDay: PreachingScheduleDay) {
+    this.preachingSchedule.updateFixedDay(fixedDay).subscribe();
+  }
+
+  onEditSpecialDate(date: SpecialPreachingDay) {
+    throw new Error('Method not implemented.');
+  }
 }
