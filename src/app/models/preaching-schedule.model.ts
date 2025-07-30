39255f2690451628ad078816c6970c7f8fcefe18
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2025-07-29 23:49:11 -0300

    feat(preaching-schedule): add special days

--- src/app/models/preaching-schedule.model.ts
+++ src/app/models/preaching-schedule.model.ts
@@ -1,5 +1,16 @@
 export interface PreachingSchedule {
+    specialDays: SpecialPreachingDay[],
     fixedPreachingDays: PreachingScheduleDay[]
+}
+
+export interface SpecialPreachingDay {
+    date: string;
+    hour: string;
+    fieldOverseer: string;
+    place: {
+        name: string;
+        adress: string;
+    };
 }
 
 export interface PreachingScheduleDay {
--- src/app/services/preaching-schedule/preaching-schedule.service.ts
+++ src/app/services/preaching-schedule/preaching-schedule.service.ts
@@ -1,23 +1,35 @@
 import { Injectable } from '@angular/core';
 import { PreachingSchedule, PreachingScheduleDay } from '../../models/preaching-schedule.model';
-import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, filter, map, Observable, switchMap, tap } from 'rxjs';
 import { Api1Service } from '../api1service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreachingScheduleService{
-
-  $reload = new BehaviorSubject(null)
+  private readonly $reload = new BehaviorSubject(null);
+  private readonly _$data = new BehaviorSubject<PreachingSchedule | null> (null);
 
   constructor(
     private readonly api: Api1Service){
+      this.$reload.pipe(
+        switchMap(() => this.api.get<PreachingSchedule>('preachingschedule/', true)),
+        tap(x=> this._$data.next(x)))
+        .subscribe();
   }
 
-  get data$() : Observable<PreachingSchedule> {
-    return this.$reload.pipe(
-      switchMap(() => this.api.get<PreachingSchedule>('preachingschedule/', true))
-    );
+  private get data$() : Observable<PreachingSchedule> {
+    return this._$data.asObservable().pipe(filter(x=> !!x)) as any;
+  }
+
+  get fixedDays$() {
+    return this.data$.pipe(
+        map((data) => data.fixedPreachingDays));
+  }
+
+  get specialDays$(){
+     return this.data$.pipe(
+        map((data) => data.specialDays));
   }
 
   updateFixedDay(fixedDay: PreachingScheduleDay) {
